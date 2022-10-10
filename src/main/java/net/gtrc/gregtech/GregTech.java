727d1cdb5ed7390c79cecf3d9ec4d0b44259e0ca
JackTench <jack.tench00@gmail.com>
2022-10-10 22:38:00 +0100

    Removed a comment that had been hanging around for some time. Don't mind me.

--- src/main/java/net/gtrc/gregtech/GregTech.java
+++ src/main/java/net/gtrc/gregtech/GregTech.java
@@ -12,7 +12,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(GregTech.MOD_ID)
 public class GregTech {
 
